357e81081a35ac6374fb4b5d0718d12936ca724c
Srikanth Marrapu <srikanth23111995@gmail.com>
2022-11-24 17:50:49 +0530

    service and repository is added

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,25 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    
+    constructor(public messagesService:MessagesService){}
     @Get()
     listMessages(){
-        return 'listMessages'
+        return this.messagesService.findAll()
     }
     @Post()
     createMessage(@Body() body:CreateMessageDto){
-        return body
+        return this.messagesService.create(body.content)
 
 
     }
     @Get('/:id')
-    getMessage(@Param() id:String){
-        return id
+    async getMessage(@Param('id') id:string){
+        const message= await this.messagesService.findOne(id)
+        return !message?new NotFoundException('message id not found'):message
 
 
     }
